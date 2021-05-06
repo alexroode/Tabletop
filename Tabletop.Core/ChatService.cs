0fed430a2832943df0e70d64224ea8403d012383
Alexander Roode <alexroode@gmail.com>
2021-05-06 15:30:18 -0700

    Add project files.

---
+++ Tabletop.Core/ChatService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tabletop.Core.Chat
+{
+    public class ChatService
+    {
+        private const int _latestMessageCount = 50;
+
+        private readonly List<ChatMessage> _messages = new();
+
+        public List<ChatMessage> GetLatestMessages()
+        {
+            return _messages.TakeLast(_latestMessageCount).ToList();
+        }
+
+        public void AddMessage(ChatMessage message)
+        {
+            _messages.Add(message);
+        }
+    }
+}
---
+++ Tabletop.Server/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabletop.Core.Chat;
+
+namespace Tabletop.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatService _chatService;
+
+        public ChatHub(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            _chatService.AddMessage(new ChatMessage() { Author = user, Text = message });
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var messages = _chatService.GetLatestMessages();
+            await Clients.Caller.SendAsync("ReceiveMessages", messages);
+            await base.OnConnectedAsync();
+        }
+    }
+}
