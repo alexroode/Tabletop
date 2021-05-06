5bbfd3c4e7ae7452dc05f52faa2f8cbef7fb036d
Alexander Roode <alexroode@gmail.com>
2021-05-06 16:46:59 -0700

    Add chat scrolling when messages received

---
+++ Tabletop.Client/wwwroot/js/interop.js
@@ -0,0 +1,9 @@
+﻿function isChatScrolledToBottom() {
+    var scroller = document.getElementById("chat-scroller");
+    return scroller.scrollHeight - scroller.scrollTop - scroller.clientHeight < 1;
+}
+
+function scrollChatToBottom() {
+    var scroller = document.getElementById("chat-scroller");
+    scroller.scrollTop = scroller.scrollHeight;
+}
